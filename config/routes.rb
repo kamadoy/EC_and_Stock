d143ed68b7009e0f27ff919e505628d6965dabdc
EC2 Default User <ec2-user@ip-172-31-31-154.ec2.internal>
2022-03-24 12:59:11 +0000

    Device_setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # 顧客用
+# URL /customers/sign_in ...
+devise_for :customers, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, controllers: {
+  registrations: "admin/registrations",
+  sessions: "admin/sessions"
+}
+
+# 店舗用
+# URL /store/sign_in ...
+devise_for :store, controllers: {
+  registrations: "store/registrations",
+  sessions: "store/sessions"
+}
 end
